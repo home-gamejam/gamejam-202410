73c4b67132aaba4ceaaae19673cba1b03bce7fb0
Gage Bovi <gage.bovi@gmail.com>
2024-10-26 15:28:33 -0500

    animimated art v1

--- smack/Scene/hero.gd
+++ smack/Scene/hero.gd
@@ -10,9 +10,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var directionX := Input.get_axis("ui_left", "ui_right")
+	if directionX:
+		velocity.x = directionX * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
@@ -21,5 +21,19 @@ func _physics_process(delta: float) -> void:
 		velocity.y = directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	
+	
+	if directionX != 0:
+		if directionX < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+			
+		$AnimatedSprite2D.play("left")
+	elif directionY < 0:
+		$AnimatedSprite2D.play("up")
+	elif directionY > 0:
+		$AnimatedSprite2D.play("down")
+	else:
+		$AnimatedSprite2D.play("idle")
 	move_and_slide()
